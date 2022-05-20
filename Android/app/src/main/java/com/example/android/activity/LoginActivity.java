01b7f35cdf25169305b1fe1fefe3f5c1df41e1e6
Jacques <mamiharilalajacques@gmail.com>
2022-05-20 22:23:56 +0300

    202205202223

--- Android/app/src/main/java/com/example/android/activity/LoginActivity.java
+++ Android/app/src/main/java/com/example/android/activity/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.android.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.android.R;
 
@@ -12,5 +15,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button inscription = findViewById(R.id.loginInscription);
+        inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, InscriptionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
